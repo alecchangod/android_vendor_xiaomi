fd2bfc718bdb31ff4ba32109e1ccdfb86de45866
johnmart19 <johnivan19999@gmail.com>
2023-06-30 10:34:07 +0300

    sm7250-common: Import Qualcomm CameraX Vendor Extensions

--- sm7250-common/sm7250-common-vendor.mk
+++ sm7250-common/sm7250-common-vendor.mk
@@ -878,4 +878,5 @@ PRODUCT_PACKAGES += \
     c2_manifest_vendor \
     manifest_android.hardware.drm@1.3-service.widevine \
     manifest_vendor.xiaomi.hardware.mlipay \
-    vendor.qti.gnss@4.0-service
+    vendor.qti.gnss@4.0-service \
+    camerax-vendor-extensions
